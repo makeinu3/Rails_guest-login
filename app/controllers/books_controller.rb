e035aafec59321170f8e73dd192bc50f28ce34ae
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-09-18 06:14:05 +0000

    [Fix] headerアイコン･投稿機能修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -17,6 +13,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def edit

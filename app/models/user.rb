8731bfa36b7ad55a0f135f29ba1860a17b972ee5
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-10-30 03:18:16 +0000

    [Start] ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,11 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [100,100]).processed
   end
 
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 end
